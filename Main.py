c8a7e3a5003e7be4ae33275a3dcad87686177b0e
Tatiana <Rybalkinatr@gmail.com>
2023-04-11 14:29:40 +0400

    Домашка 1 сделана

---
+++ Main.py
@@ -0,0 +1,15 @@
+
+a = int (input ('Введите число a '))
+b = int (input ('Введите число b '))
+
+if a > b:
+
+    print (f"a = {a} больше b = {b}")
+
+elif a < b:
+
+    print (f"a = {a} меньше b = {b}")
+
+else:
+
+    print ("==")
